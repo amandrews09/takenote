4610ea65245d7cd0a93e60f47428999b7d65af43
Amandrews <amandrews09@gmail.com>
2024-03-17 12:05:06 -0600

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Note Taker Starter Code
+# Take Note
+Module 11 Challenge: Express.js Challenge: Note Taker
+
+## Description 
+As a busy professional, I want to use a note-taking app to keep track of my thoughts, tasks, and ideas, so that I can stay organized and easily access my notes whenever I need them.
+
+## Deployed URL
+https://takenote-5y0h.onrender.com 
+
+## Installation
+Crate a package.json by running "npm init"
+Install Node.js
+Create a .gitignore files and include "node_modules", "package-lock.json", and ".vscode"
+Install express by running "npm install express"
+
+## Usage
+Right click on "server.js" to open in integrated terminal. Initiate application by entering "node server.js"
+[Demonstration](./TakeNote.gif)
+
+## Credits
+Help from chatGPT and Michael Chiem as well as starter code from https://github.com/coding-boot-camp/miniature-eureka
+
+## License
+MIT
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
+// Importing the express framework to create the server
 const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 3001
+const app = express(); // Creating an instance of the Express application
+const path = require('path'); // Module for working with file and directory paths
+const PORT = process.env.PORT || 3001 // Setting the port for the server
 
+// Importing route modules
 const notesRoutes = require('./routes/notesRoutes');
 const indexRoutes = require('./routes/indexRoutes');
 
+// Setting the path to the public directory
 const publicPath = path.join(__dirname, 'public');
 
+// Middleware to parse incoming JSON requests
 app.use(express.json());
+
+// Middleware to parse incoming URL-encoded requests
 app.use(express.urlencoded({ extended: true }));
+
+// Serving static files from the public directory
 app.use(express.static(publicPath));
 
+// Assigning routes for the root path and the notes path
 app.use('/', indexRoutes);
 app.use('/', notesRoutes);
 
+// Starting the server and listening for incoming requests
 app.listen(PORT, () =>
     console.log(`Listening for requests on port ${PORT}!`)
 );
